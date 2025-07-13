37ec0f1ae9f2bed1bf1fef9a2c3c4687197e3ff9
sidpd51 <sidpd510@gmail.com>
2025-07-13 22:41:51 +0530

    feat: fetch new access token from server response and set the accessToken & redirect to error pages on the basis of status of error

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,5 +1,6 @@
 import { useAuth } from "@/context/AuthContext";
 import axios from "axios";
+import { useNavigate } from "react-router";
 
 const api = axios.create({
     baseURL: 'http://localhost:3542/api/v1',
@@ -7,14 +8,38 @@ const api = axios.create({
 });
 
 export const useApi = () => {
-    const { accessToken } = useAuth();
+    const { accessToken, setAccessToken } = useAuth();
+    const navigate = useNavigate();
 
+    // Request interceptor: Add Authorization header
     api.interceptors.request.use((config) => {
         if (accessToken) {
             config.headers.Authorization = `Bearer ${accessToken}`;
         }
         return config;
     });
+
+    api.interceptors.response.use(
+        (response) => {
+            const newAccessToken = response.headers["x-new-access-token"];
+            console.log("Before accessToken: ", accessToken);
+            if (newAccessToken) {
+                setAccessToken(newAccessToken);
+                console.log("After accessToken: ", accessToken);
+            }
+            return response;
+        },
+        (error) => {
+            const status = error.response?.status;
+
+            if (status === 401) navigate("/unauthorized")
+            else if (status === 403) navigate("/forbidden")
+            else if (status === 404) navigate("/not-found")
+            else navigate('/error')
+
+            return Promise.reject(error);
+        }
+    );
 
     return api;
 }
