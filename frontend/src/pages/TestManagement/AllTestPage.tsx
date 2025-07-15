d33a70fd8b3155dd5432ee9d88a02bfc192d9875
sidpd51 <sidpd510@gmail.com>
2025-07-15 20:50:31 +0530

    feat: Add AllTestPage component to render tests with shimmer ui effect

--- frontend/src/pages/TestManagement/AllTestPage.tsx
+++ frontend/src/pages/TestManagement/AllTestPage.tsx
@@ -1,9 +1,60 @@
+import ShimmerCard from "@/components/ShimmerCard";
+import TestCard from "@/components/TestCard"
+import axios from "axios";
+import { useEffect, useRef, useState } from "react";
+
+const BASE_URL = "https://dummyjson.com/posts?limit=51";
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
 
 
 const AllTestPage = () => {
+  const [error, setError] = useState();
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>();
+
+  const abortControllerRef = useRef<AbortController | null>(null);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      abortControllerRef.current?.abort();
+      abortControllerRef.current = new AbortController();
+
+      setIsLoading(true)
+      try {
+        const response = await axios.get(BASE_URL, {
+          signal: abortControllerRef.current?.signal
+        });
+        setPosts(response.data.posts);
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (e: any) {
+        setError(e);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchPosts();
+  }, []);
+
+  if (error) {
+    return <div>Something went wrong! Please try again.</div>
+  }
+
   return (
-    <div>
-      all test page
+    <div id="all-test-page">
+      <div className="grid grid-cols-3 gap-4">
+        {isLoading ?
+          Array.from({ length: 8 }).map((_, i) => <ShimmerCard key={i} />) :
+          posts.map((post) => {
+            return <TestCard key={post.id} title={post.title} description={post.body} />
+          })
+        }
+      </div>
     </div>
   )
 }
